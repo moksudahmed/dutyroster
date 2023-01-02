e5274569110de1b754aa22a0e4adfc0d756c2291
moksudahmed <k.moksud@gmail.com>
2023-01-02 15:24:26 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,24 @@ def calculate(a):
     return count
 
 def swap(arr,a,b, limit):
+    pos = 0
     for i in range(len(arr)):
         if calculate(arr[i]) >= limit:
             for j in range(len(arr[i])):
-                if arr[i][j] == 1 and calculate(arr[i]) >limit:
-                    arr[i][j] = 0
-                    if j<=len(b):
-                        arr[b[0][j]][j] = 1
-           # print(arr[i], calculate(arr[i]), b[0][2])    
+                if arr[i][j] == 1 and calculate(arr[i]) >limit:                    
+                    if j<=len(b):                
+                        arr[a[0][j]][a[0][j]] = 0
+                        arr[b[0][j]][a[0][j]] = 1
+                        print(i,j)
+           
     return arr
 
 limit = 3
-a = [[1,3,5,6],[4,4,4,5]]
-b = [[0,2,4],[2,2,2]]
+a = [[3,5,6],[4,4,5]]
+b = [[0,4],[2,2]]
 arr = [[0,1,0,0,0,1,0,0,0,0],
-       [0,1,0,1,0,1,0,0,0,1],
-       [0,0,0,1,0,0,0,1,0,0],
+       [0,0,0,1,0,1,0,0,0,1],
+       [0,1,0,1,0,0,0,1,0,0],
        [0,0,0,1,0,0,1,1,0,1],
        [0,0,0,1,0,1,0,0,0,0],
        [0,0,0,1,0,1,0,0,1,1],
